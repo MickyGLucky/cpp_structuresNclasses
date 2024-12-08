141cec6843fdd3769c1ecedb3b2565cf2ca99f14
MickyGLucky <ma0826@saskpolytech.ca>
2024-12-08 14:42:16 -0600

    Did the practice

---
+++ README.md
@@ -0,0 +1,94 @@
+# C++ structures and classes
+
+In this practic, I will be focuing on learning Enumerated Types, Unions and Structures, and Classes.
+
+> Enumerated types and unions are “variants”, i.e., “or”s.
+> Structures are “non-variant”, i.e., “and”s
+
+## Enumerated types and unions
+Types can be user-defined through an enumeerated type, union or structure.
+- **Enumerated types**. Often used in conjuntion with a switch statement
+
+    **Sample code** 
+    ```cpp
+    typedef enum Level{
+        LOW = 1,
+        MEDIUM,
+        HIGH
+    };
+    enum Level lv = MEDIUM;
+    switch (myVar){
+        case LOW:
+            ...;
+            break;
+        ...
+    }
+    ```
+
+- **Unions**.  Can store multiple different types in the same memory location. Only one member can store data at an instance.
+    **Sample code**
+    ```cpp
+    typedef union Connection {
+        int networkID;
+        char networkName[4];
+    }
+    Connection id = 443;
+    Connection ssid = "wififree";
+
+    ```
+
+- **Structure**. 
+    ❑Comprise a collection or group of values.
+    ❑As a type: can be thought of as a header in Excel.
+    ❑As a value: can be thought of as a single row under that header.
+    ❑Read as “and’s”
+
+    **Example** 
+    ```cpp
+        typedef struct IPAddr {
+            unsigned char Addr_1st;
+            unsigned char Addr_2nd;
+            unsigned char Addr_3rd;
+            unsigned char Addr_4th;
+        };
+
+        IPAddr ip;
+        ip.Addr_1st = 172;
+        ip.Addr_2nd =  16;
+        ip.Addr_3rd =  1 ;
+        ip.Addr_4th =  205;   
+    ```
+    
+    
+    | Name       | Value |
+    |-----------|-------|
+    | Addr_1st      | 172 |
+    | Addr_2nd    | 16|
+    | Addr_3rd      | 1 |
+    | Addr_4th    | 205|    
+
+    E.g., 172.16.1.205    
+        
+    
+
+
+
+## Class
+Classes extend the concept of a structure.
+Class similar to structures, have method, i.e., function members(methods and properties are collectively called members), vaule members are called properties.
+
+**exameple**
+```cpp
+ class ClassName {
+ protected:
+ …
+ private:
+ …
+ public:
+ …
+ };
+
+```
+
+
+## Template
---
+++ SturcturesNclasses/SturcturesNclasses.cpp
@@ -0,0 +1,149 @@
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+
+typedef enum State {
+	ENABLED ,
+	FOWARD ,
+	BACKWARD ,
+	STOP ,
+	DISABLED 
+};
+
+typedef union Connection{
+	int networkID;
+	char networkName[4];
+};
+
+typedef struct IPAddr {
+	unsigned char Addr_1st;
+	unsigned char Addr_2nd;
+	unsigned char Addr_3rd;
+	unsigned char Addr_4th;
+};
+
+// Employee class
+class Employee
+{
+public:
+	// Constructor
+	Employee(std::string name, int id)
+	{
+		m_name = name;
+		m_id = id;
+	}
+
+	// Public accessors
+	std::string get_name() { return m_name; }
+	int get_id() { return m_id; }
+protected:
+	// Private data for employee
+	// Can only be set through constructor
+	std::string m_name;
+	int m_id;
+};
+
+// Manager is a superclass of an employee
+class Manager : public Employee
+{
+public:
+	// Use base class constructor
+	Manager(std::string name, int id) : Employee(name, id) {}
+
+	// Special manager function to add subordinates
+	void add_subordinate(Employee employee)
+	{
+		m_manages.push_back(employee);
+	}
+
+	// Special manager report
+	void report()
+	{
+		std::cout << std::setw(30) << std::right << "Manager: " << std::setw(10) << std::left << " " << m_name << std::endl;
+		std::cout << std::setw(30) << std::right << "ID: " << std::setw(10) << std::left << " " << m_id << std::endl;
+		std::cout << std::endl;
+		std::cout << std::setw(30) << std::right << "Manages:" << std::endl;
+		std::cout << std::setw(30) << std::right << "========" << std::endl << std::endl;
+
+		// Iterate all subordinates
+		for (Employee e : m_manages)
+		{
+			std::cout << std::setw(30) << std::right << e.get_name() << std::setw(10) << std::left << " " << e.get_id() << std::endl;
+		}
+	}
+private:
+	// Private data for manager
+	std::vector<Employee> m_manages;
+};
+
+
+
+void movement(enum State state) {
+	switch (state) {
+	case ENABLED:
+		std::cout << "Engine started" << std::endl;
+		break;
+	case FOWARD:
+		std::cout << "Car go forward" << std::endl;
+		break;
+	case BACKWARD:
+		std::cout << "Car go backward" << std::endl;
+		break;
+	case STOP:
+		std::cout << "Car stropped" << std::endl;
+		break;
+	case DISABLED:
+		std::cout << "Engine is off" << std::endl;
+		break;
+	default:
+		std::cout << "ERROR" << std::endl;
+		break;
+	}
+}
+
+int main()
+{
+	State state = STOP;
+	state = FOWARD;
+	movement(state);
+	state = BACKWARD;
+	movement(state);
+
+	Connection net;
+	net.networkID = 443;
+	std::cout << "Network ID: " << net.networkID << std::endl;
+	net.networkName[0] = 'T';
+	net.networkName[1] = 'C';
+	net.networkName[2] = 'P';
+	net.networkName[3] = '\0';
+	std::cout << "Network Name: " << net.networkName << std::endl;
+
+	IPAddr ip;
+	ip.Addr_1st = 172;
+	ip.Addr_2nd = 16;
+	ip.Addr_3rd = 1;
+	ip.Addr_4th = 205;
+	std::cout << "IP Address: " << (int)ip.Addr_1st << "." << (int)ip.Addr_2nd << "." << (int)ip.Addr_3rd << "." << (int)ip.Addr_4th << std::endl;
+
+	// Employees
+	Employee linda = Employee("Linda Shmer", 5689);
+	Employee carol = Employee("Carol Cannavan", 9823);
+	Employee mark = Employee("Mark Williamsonsford", 4789);
+
+	// Manager
+	Manager dave = Manager("Dave Field", 7589);
+
+	// Setup data
+	dave.add_subordinate(linda);
+	dave.add_subordinate(carol);
+	dave.add_subordinate(mark);
+
+	// Provide report
+	dave.report();
+
+}
+
